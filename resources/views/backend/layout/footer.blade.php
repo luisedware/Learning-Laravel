83cae46c243b9409d1e139a3e5ef61521816d161
luisedware <luisedware@qq.com>
2016-03-21 20:59:51 +0800

    Require all granted

--- resources/views/backend/layout/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        Anything you want
-    </div>
-    <strong>Copyright © {{date('Y')}} <a href="#">Company</a>.</strong> All rights reserved.
-</footer>
