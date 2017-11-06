65ff8a0be1d7351a48514d10f3eba666a68331f2
luisedware <luisedware@qq.com>
2017-11-07 06:31:14 +0800

    Generated topics

--- LaraBBSTutorialSample/routes/web.php
+++ LaraBBSTutorialSample/routes/web.php
@@ -18,3 +18,4 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 // Users
 Route::resource('users', 'UsersController');
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
