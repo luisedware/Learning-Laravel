13dfb7ab213d847aa637bd1fb355823a26f4139b
luisedware <luisedware@qq.com>
2017-11-07 10:31:33 +0800

    Author can delete topic

--- LaraBBSTutorialSample/app/Models/User.php
+++ LaraBBSTutorialSample/app/Models/User.php
@@ -32,9 +32,13 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-
     public function topics()
     {
         return $this->hasMany(Topic::class);
+    }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
     }
 }
